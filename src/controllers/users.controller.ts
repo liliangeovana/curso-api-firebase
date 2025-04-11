a106a79c64b8c801e696eb68d66e7ecd662df75f
Lílian Saraiva <lilian.botelho@ae3tecnologia.com.br>
2025-04-11 11:56:54 -0400

    atualizando métodos - conectados ao firebase

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,56 +1,76 @@
 import { Request, Response } from 'express';
+import { getFirestore } from 'firebase-admin/firestore';
 
 // Tipo de usuário
-type User =
-  {
-    id: number;
-    nome: string;
-    email: string;
-  };
-let id = 0;
-const usuarios: User[] = [];
+type User = {
+  id: number;
+  nome: string;
+  email: string;
+};
 
 export class UsersControllers {
 
-  static getAll(req: Request, res: Response) {
-    res.send(usuarios);
+  static async getAll(req: Request, res: Response) {
+    // .get() - Executa a consulta e retorna os resultados como um QuerySnapshot (tipo uma "foto" da coleção no momento da consulta)
+    // .get() - retorna uma Promise, logo usamos await para esperar a resposta
+
+    const snapshot = await getFirestore().collection('users').get();
+
+    //docs - o snapshot contém uma lista de documentos (QueryDocumentSnapshot)
+    // data - retorna todos os dados do documento
+    const users = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      }
+    })
+
+    res.send(users);
   }
 
-  static getById(req: Request, res: Response) {
-    const userId = Number(req.params.id);
-    const user = usuarios.find(user => user.id === userId);
+  static async getById(req: Request, res: Response) {
+    const userId = req.params.id;
+    
+    const doc = await getFirestore().collection('users').doc(userId).get();
+
+    const user = {
+      id: doc.id,
+      ...doc.data(),
+    }
+
     res.send(user)
   }
 
-  static create(req: Request, res: Response) {
+  static async create(req: Request, res: Response) {
     const user = req.body;
-    user.id = ++id
   
-    usuarios.push(user);
+    // .add() - Adiciona um novo documento a coleção gerando um id automaticamente
+    await getFirestore().collection('users').add(user);
+
     res.send({
       message: 'Usuário adicionado com sucesso',
     })
   }
 
   static update(req: Request, res: Response) {
-    const userId = Number(req.params.id); // pega o id do usuário
-    const user = req.body // pega o corpo da requisição
+    const userId = req.params.id; // pega o id do usuário
+    const user = req.body as User // pega o corpo da requisição
   
-    const indexOf = usuarios.findIndex((u: User) => u.id === userId); // encontra o índice do usuário
-  
-    usuarios[indexOf].nome = user.nome; // atualiza o nome do usuário
-    usuarios[indexOf].email = user.email; // atualiza o email do usuário
+    // set() - Atualiza o documento com o id do usuário
+    getFirestore().collection('users').doc(userId).set({
+      nome: user.nome,
+      email: user.email,
+    });
   
     res.send({
       message: 'Usuário atualizado com sucesso',
     });
   }
 
-  static delete(req: Request, res: Response) {
-    const userId = Number(req.params.id); // pega o id do usuário
+  static async delete(req: Request, res: Response) {
+    const userId = req.params.id; // pega o id do usuário
   
-    const indexOf = usuarios.findIndex((u: User) => u.id === userId); // encontra o índice do usuário
-    usuarios.splice(indexOf, 1); // remove o usuário do array
+   await getFirestore().collection('users').doc(userId).delete(); // deleta o usuário
   
     res.send({
       message: 'Usuário removido com sucesso',
