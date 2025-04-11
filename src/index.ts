15aa56c1ffec7e5e2715ebb01b67ff6254227e05
Lílian Saraiva <lilian.botelho@ae3tecnologia.com.br>
2025-04-11 10:44:23 -0400

    configuração inicial do firebase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { initializeApp } from 'firebase-admin/app';
 import { routes } from './routes/index';
 
+initializeApp();
 const app = express();
+
 routes(app);
 
 app.listen(3000, () => {
