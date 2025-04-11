2df1a2b09d459b60d84bbbdfb1f8f2f46805e729
Lílian Saraiva <lilian.botelho@ae3tecnologia.com.br>
2025-04-11 16:35:42 -0400

    inserindo try/catch

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -11,69 +11,107 @@ type User = {
 export class UsersControllers {
 
   static async getAll(req: Request, res: Response) {
-    // .get() - Executa a consulta e retorna os resultados como um QuerySnapshot (tipo uma "foto" da coleção no momento da consulta)
-    // .get() - retorna uma Promise, logo usamos await para esperar a resposta
+    try {
+      // .get() - Executa a consulta e retorna os resultados como um QuerySnapshot (tipo uma "foto" da coleção no momento da consulta)
+      // .get() - retorna uma Promise, logo usamos await para esperar a resposta
 
-    const snapshot = await getFirestore().collection('users').get();
+      const snapshot = await getFirestore().collection('users').get();
 
-    //docs - o snapshot contém uma lista de documentos (QueryDocumentSnapshot)
-    // data - retorna todos os dados do documento
-    const users = snapshot.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data(),
-      }
-    })
+      //docs - o snapshot contém uma lista de documentos (QueryDocumentSnapshot)
+      // data - retorna todos os dados do documento
+      const users = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        }
+      })
 
-    res.send(users);
+      res.send(users);
+
+    } catch (error) {
+
+      res.status(500).send({
+        message: 'Erro interno do servidor.'
+      });
+    }
+
   }
 
   static async getById(req: Request, res: Response) {
-    const userId = req.params.id;
+    try {
+      const userId = req.params.id;
     
-    const doc = await getFirestore().collection('users').doc(userId).get();
+      const doc = await getFirestore().collection('users').doc(userId).get();
 
-    const user = {
-      id: doc.id,
-      ...doc.data(),
+      const user = {
+        id: doc.id,
+        ...doc.data(),
+      }
+
+      res.send(user);
+    } catch (error) {
+      res.status(500).send({
+        message: 'Erro interno do servidor.'
+      });
     }
-
-    res.send(user)
   }
 
   static async create(req: Request, res: Response) {
-    const user = req.body;
+    try {
+      const user = req.body;
   
-    // .add() - Adiciona um novo documento a coleção gerando um id automaticamente
-    await getFirestore().collection('users').add(user);
+      // .add() - Adiciona um novo documento a coleção gerando um id automaticamente
+      await getFirestore().collection('users').add(user);
 
-    res.send({
-      message: 'Usuário adicionado com sucesso',
-    })
+      res.status(201).send({
+        message: 'Usuário adicionado com sucesso',
+      })
+    } catch (error) {
+      res.status(500).send({
+        message: 'Erro interno do servidor.'
+      });
+    }
   }
 
   static update(req: Request, res: Response) {
-    const userId = req.params.id; // pega o id do usuário
-    const user = req.body as User // pega o corpo da requisição
+    try {
+
+      const userId = req.params.id; // pega o id do usuário
+      const user = req.body as User // pega o corpo da requisição
   
-    // set() - Atualiza o documento com o id do usuário
-    getFirestore().collection('users').doc(userId).set({
-      nome: user.nome,
-      email: user.email,
-    });
-  
-    res.send({
-      message: 'Usuário atualizado com sucesso',
-    });
+      // set() - Atualiza o documento com o id do usuário
+      getFirestore().collection('users').doc(userId).set({
+        nome: user.nome,
+        email: user.email,
+      });
+    
+      res.send({
+        message: 'Usuário atualizado com sucesso',
+      });
+
+    } catch (error) {
+
+      res.status(500).send({
+        message: 'Erro interno do servidor.'
+      });
+    }
+
   }
 
   static async delete(req: Request, res: Response) {
-    const userId = req.params.id; // pega o id do usuário
+    try {
+      const userId = req.params.id; // pega o id do usuário
   
-   await getFirestore().collection('users').doc(userId).delete(); // deleta o usuário
+      await getFirestore().collection('users').doc(userId).delete(); // deleta o usuário
   
-    res.send({
-      message: 'Usuário removido com sucesso',
-    });
+      // retorna 204 (sem conteúdo)
+      // .end() - finaliza a resposta sem enviar nada 
+      res.status(204).end();
+
+    } catch (error) {
+      res.status(500).send({
+        message: 'Erro interno do servidor.'
+      });
+    }
   }
 }
